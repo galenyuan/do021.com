b91da748906585d182449ec1a2fb0e80e6d0b1b7
Oliver.W <ole3021@gmail.com>
2018-07-09 20:37:23 +0800

    update GA

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-122025507-1',
+  router,
+  autoTracking: {
+    skipSamePath: true
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
