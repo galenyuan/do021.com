5bb0567cb03be6245f762f5238ec7e4e126b9ce6
Oliver.W <ole3021@gmail.com>
2018-07-09 23:15:59 +0800

    update 404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -4,7 +4,19 @@
 
 <template>
   <div>
-    <h1>404 NOT FOUND</h1>
+    <div class="content clearfix">
+      <!-- 404 Section -->
+      <div class="section-block clear-height pt-150 pb-150">
+        <div class="row">
+          <div class="column width-12 center">
+            <h1 class="status-code-title status-code color-gradient-purple-haze">404</h1>
+            <h3 class="title-medium color-color mb-40 mb-mobile-40">指定页面不存在...</h3>
+            <a href="/" class="button rounded large bkg-theme bkg-hover-theme color-white color-hover-white shadow-hover">返回首页</a>
+          </div>
+        </div>
+      </div>
+      <!-- 404 Section End -->
+    </div>
   </div>
 </template>
 
