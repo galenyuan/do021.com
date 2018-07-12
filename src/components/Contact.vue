c902c3e488d026f5052dc23cb91349170633ae7f
Oliver.W <ole3021@gmail.com>
2018-07-13 00:19:43 +0800

    update: contact field name

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -8,18 +8,18 @@
     <div class="column width-6 offset-3 center">
       <h3 class="mb-30 color-white">{{ STRING.title }}</h3>
       <div class="register-form-container">
-        <form class="register-form" action="#" method="post" novalidate="">
+        <form class="register-form" action="mailto:ole3021@gmail.com" method="post" novalidate="">
           <div class="row merged-form-elements">
             <div class="column width-6">
               <div class="field-wrapper">
                 <label class="color-white">{{ STRING.firstName }}:</label>
-                <input type="text" name="contact[firstname]" class="form-firstname form-element rounded medium center" placeholder="某某" required="">
+                <input type="text" name="firstname" class="form-firstname form-element rounded medium center" placeholder="某某" required="">
               </div>
             </div>
             <div class="column width-6">
               <div class="field-wrapper">
                 <label class="color-white">{{ STRING.lastName }}:</label>
-                <input type="text" name="contact[lastname}" class="form-lastname form-element rounded medium center" placeholder="王" required="">
+                <input type="text" name="lastname" class="form-lastname form-element rounded medium center" placeholder="王" required="">
               </div>
             </div>
           </div>
@@ -27,19 +27,20 @@
             <div class="column width-12">
               <div class="field-wrapper">
                 <label class="color-white">{{ STRING.contact }}:</label>
-                <input type="email" name="contact[contact]" class="form-email form-element rounded medium center" placeholder="有效的邮箱或者手机号码" required="">
+                <input type="email" name="contact" class="form-email form-element rounded medium center" placeholder="有效的邮箱或者手机号码" required="">
               </div>
             </div>
             <div class="column width-12">
               <div class="field-wrapper">
                 <label class="color-white">{{ STRING.message }}</label>
-                <textarea  rows="6" name="contact[message]" class="form-password form-element rounded medium center" placeholder="这里留下你的消息，我们会尽快回复。" required="">
+                <textarea  rows="6" name="message" class="form-password form-element rounded medium center" placeholder="这里留下你的消息，我们会尽快回复。" required="">
                 </textarea>
               </div>
             </div>
             <div class="column width-12 mt-10">
               <input type="submit" :value="STRING.submit" class="form-submit button rounded medium border-white bkg-hover-blue-light bkg-focus-green color-white color-hover-white no-margins">
             </div>
+            <p class="color-white">自动提交功能添加中</p>
           </div>
         </form>
         <div class="form-response show"></div>
